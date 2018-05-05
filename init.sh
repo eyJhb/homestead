fa3d031a6f7f191d6dc2e1ed01f4dc6c7d52cba9
eyJhb <eyjhbb@gmail.com>
2018-05-05 14:14:30 +0200

    added forward.sh and updated init.sh to copy this

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ mkdir -p "$homesteadRoot"
 
 cp -i src/stubs/Homestead.yaml "$homesteadRoot/Homestead.yaml"
 cp -i src/stubs/after.sh "$homesteadRoot/after.sh"
+cp -i src/stubs/forward.sh "$homesteadRoot/forward.sh"
 cp -i src/stubs/aliases "$homesteadRoot/aliases"
 
 echo "Homestead initialized!"
